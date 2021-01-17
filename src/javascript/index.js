af6c361eecc092dd14de4c7b30c79838f972da63
Michał Fal <michfal86@gmail.com>
2021-01-17 17:21:20 +0100

    search functionality

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,8 +2,6 @@ import '../sass/style.scss';
 
 import "regenerator-runtime/runtime.js";
 import {getWeather} from './getData';
-// import {getCoordinates} from './getData';
-// import {getWeatherForCity} from './getData';
 import {displayCurrentWeather} from './displayData';
 import {displayDailyWeather} from './displayData';
 import {currentTime} from './displayData'
@@ -11,38 +9,19 @@ import {getSearchFormData} from './searchForm';
 import {weatherIcons} from './weatherIcons'
 
 
-
-
-// console.log(weatherIcons["01d"]);
-
 const searchButton = document.querySelector('#search_button');
 
-window.addEventListener('load', async (event) => {
+
+searchButton.addEventListener('click', async (event) => {
     
     event.preventDefault()
     const formData = getSearchFormData();
-    // const country = formData.country
     const city = formData
-    // console.log(city)
     
     const weatherData = await getWeather(city, process.env.API_KEY);
-    // console.log(weatherData)
     await displayCurrentWeather(weatherData, weatherIcons)
     await displayDailyWeather(weatherData, weatherIcons)
 })
-
-// searchButton.addEventListener('click', async (event) => {
-    
-//     event.preventDefault()
-//     const formData = getSearchFormData();
-//     // const country = formData.country
-//     const city = formData
-//     // console.log(city)
-    
-//     const weatherData = await getWeather(city, key);
-//     await displayCurrentWeather(weatherData)
-//     await displayDailyWeather(weatherData)
-// })
 
 
 
